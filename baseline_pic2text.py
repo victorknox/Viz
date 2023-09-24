db3bb926969471c746b62036f8d3c03f3ab6f7ec
alexjbusch <alexanderjbusch@gmail.com>
2023-09-23 20:07:41 -0400

    added baseline model

---
+++ baseline_pic2text.py
@@ -0,0 +1,20 @@
+import requests
+import torch
+from PIL import Image
+from transformers import BlipProcessor, BlipForQuestionAnswering
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"running on {device}...")
+
+processor = BlipProcessor.from_pretrained("Salesforce/blip-vqa-base")
+model = BlipForQuestionAnswering.from_pretrained("Salesforce/blip-vqa-base")
+
+img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
+raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
+question = "what are they doing?"
+inputs = processor(raw_image, question, return_tensors="pt")
+
+out = model.generate(**inputs)
+generated_text = processor.decode(out[0], skip_special_tokens=True)
+print(generated_text)

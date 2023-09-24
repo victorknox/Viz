03879d9de97869362e1773d84d051ec17d7aa258
alexjbusch <alexanderjbusch@gmail.com>
2023-09-23 23:23:09 -0400

    added repeatable photo taking

--- tts.py
+++ tts.py
@@ -5,12 +5,26 @@ from gtts import gTTS
 # This module is imported so that we can
 # play the converted audio
 import os
+from playsound import playsound
 
 
 
 def read_str(readthis):
+
+    if os.path.exists("vizproject.mp3"):
+        os.remove("vizproject.mp3")
+    
     myobj = gTTS(text=readthis, lang='en', slow=False)
     myobj.save("vizproject.mp3")
-    os.system("afplay vizproject.mp3")
 
+    #mac
+    #os.system("afplay vizproject.mp3")
+
+    #windows
+    #os.system("ffmpeg vizproject.mp3")
+
+
+    playsound('vizproject.mp3')
+    #linux
+    #os.system("vizproject.mp3")
 

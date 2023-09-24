2551f0f57bededa9d830ec994b5eb2455c552439
victorknox <victorknox@pop-os.localdomain>
2023-09-23 23:21:34 -0400

    added blip2 code

---
+++ blip.py
@@ -0,0 +1,22 @@
+# pip install accelerate bitsandbytes
+import torch
+import requests
+from PIL import Image
+from transformers import Blip2Processor, Blip2ForConditionalGeneration
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+print(f"running on {device}...")
+
+processor = Blip2Processor.from_pretrained("Salesforce/blip2-opt-2.7b")
+model = Blip2ForConditionalGeneration.from_pretrained("Salesforce/blip2-opt-2.7b", load_in_8bit=True, device_map="auto")
+
+# img_url = 'https://storage.googleapis.com/sfr-vision-language-research/BLIP/demo.jpg' 
+# raw_image = Image.open(requests.get(img_url, stream=True).raw).convert('RGB')
+
+def talk_to_img(input_image, question = "question: describe this picture, Answer: "):
+    
+    inputs = processor(input_image, question, return_tensors="pt").to("cuda", torch.float16)
+    generated_ids = model.generate(**inputs)
+    generated_text = processor.batch_decode(generated_ids, skip_special_tokens=True)[0].strip()
+    print(generated_text)
+    return generated_text
